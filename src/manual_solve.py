dbd0c8f58cf6e5da3b253c32d2db6571e3b2fd7f
rishi03021994 <r.gupta4@nuigalway.ie>
2020-12-12 16:19:43 +0000

    Added comments and task descriptions for task ids c9e6f938 and c1d99e64 for manual_solve.py file

--- src/manual_solve.py
+++ src/manual_solve.py
@@ -10,30 +10,33 @@ import re
 ### result. Name them according to the task ID as in the three
 ### examples below. Delete the three examples. The tasks you choose
 ### must be in the data/training directory, not data/evaluation.
+
+'''Task ID c9e6f938 Transformation Description - We have an input 3x3 grid in the particular task and each cell has either Black(0) or Orange(7) color.
+The tranformation which is needed in this task to get desired output is to reshape the matrix to 3x6 and just take the mirror image on Y-axis of the 3x3 matrix to the right
+side of the Y-axis and concatenate the mirrored 3x3 matrix with original one which will result in 3x6 matrix.'''
 def solve_c9e6f938(x):
     new_xarr = []
-    for train_x in x:
-        rev_train_x = train_x[::-1]
-        new_xarr.append(np.concatenate((train_x, rev_train_x), axis=0))
-    x = np.array(new_xarr)
+    for train_x in x: # Iterate the 3x3 array and take each row of the array
+        rev_train_x = train_x[::-1] # For each row get the reversed or the mirrored row
+        new_xarr.append(np.concatenate((train_x, rev_train_x), axis=0)) # Concatenate the original row with the reversed row using axis = 0 and append it into new list
+    x = np.array(new_xarr) # Convert the list to numpy array
     return x
+'''All the training and test grids are solved correctly for Task ID c9e6f938'''
 
+'''Task ID c1d99e64 Transformation Description - We have an input grid and the grid has two different colors either black(0) cell or any ther color.
+To tranform the grids in this task we need to find all rows and columns in grid whose all cells are black in color or have 0 in them and then 
+for those rows or columns replace all the cells with red(2) color.'''
 def solve_c1d99e64(x):
     new_xarr = x.copy()
-    rows = np.where(~new_xarr.any(axis=1))[0]
-    cols = np.where(~new_xarr.any(axis=0))[0]
-    for col in cols:
-        new_xarr[:,col] = 2
-    for row in rows:
-        new_xarr[row,:] = 2
+    rows = np.where(~new_xarr.any(axis=1))[0] # Find all rows in the grid where all cells in the row are having 0 value(black)
+    cols = np.where(~new_xarr.any(axis=0))[0] # Find all columns in the grid where all cells in the column are having 0 value(black)
+    for col in cols: # Iterate all such columns having all values as 0
+        new_xarr[:,col] = 2 # Replace the all cells value with 2 in the particular column
+    for row in rows: # Iterate all such rows having all values as 0
+        new_xarr[row,:] = 2 # Replace the all cells value with 2 in the particular row
     x = new_xarr
     return x
-
-#def solve_3631a71a(x):
-#    return x
-
-#def solve_ecdecbb3(x):
-#    return x
+'''All the training and test grids are solved correctly for Task ID c1d99e64'''
 
 
 def main():
